ff19ec4b46284877e8e17210b6785a3b63b20595
Fabian Peter Hammerle <fabian@hammerle.me>
2021-03-19 15:12:56 +0100

    entrypoint: assign public keys provided via env vars GITOLITE_USER_PUBLIC_KEY_[USER]

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,16 @@ if [ ! -d "$HOME/.gitolite" ]; then
     # > First run: either the pubkey or the admin name is *required*, [...]
     (set -x; gitolite setup --admin "$GITOLITE_INITIAL_ADMIN_NAME")
 fi
+unset GITOLITE_INITIAL_ADMIN_NAME
+
+printenv | cut -d = -f 1 | while IFS= read -r var_name; do
+    if [ "$(echo "$var_name" | cut -d _ -f -4)" = "GITOLITE_USER_PUBLIC_KEY" ]; then
+        user="$(echo "$var_name" | cut -d _ -f 5-)"
+        key_path="$HOME/container-entrypoint-user-public-keys/${user}.pub"
+        mkdir --parents "$(dirname "$key_path")"
+        (set -x; printenv "$var_name" > "$key_path"; gitolite setup --pubkey "$key_path")
+    fi
+done
 
 set -x
 
